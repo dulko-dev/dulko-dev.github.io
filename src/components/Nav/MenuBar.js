5f81df35cef16992370233dce9d78b20351ee16d
Kamil Duliniec <123dulko@wp.pl>
2021-06-28 12:59:33 +0200

    open menu after click on it and styled

---
+++ src/components/Nav/MenuBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Ul, Li } from "./style";
+
+const MenuBar = () => {
+  return (
+    <Ul>
+      <Li>Home</Li>
+      <Li>About</Li>
+      <Li>Projects</Li>
+      <Li>Contact</Li>
+    </Ul>
+  );
+};
+
+export default MenuBar;
--- src/components/Nav/style.js
+++ src/components/Nav/style.js
@@ -27,6 +27,7 @@ export const Burger = styled.div`
   background-color: black;
   border-radius: 5px;
   transition: all 0.5s ease-in-out;
+  z-index: 1;
   ${({ horizontalDash }) =>
     horizontalDash &&
     css`
@@ -50,6 +51,7 @@ export const Burger = styled.div`
       horizontalDash &&
       css`
         transform: rotate(45deg) translate(35px, -35px);
+        background-color: rgb(238, 238, 238);
       `}
   }
   &:after {
@@ -58,8 +60,55 @@ export const Burger = styled.div`
       horizontalDash &&
       css`
         transform: rotate(-45deg) translate(35px, 35px);
+        background-color: rgb(238, 238, 238);
       `}
+  }
+  &:hover:after,
+  &:hover:before {
+    background-color: rgba(238, 238, 238, 0.4);
   }
 `;
 
 export const NavLogo = styled.div``;
+
+export const Module = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  z-index: 1;
+  transform: translate(-50%, -50%);
+  transition: all 0.5s ease-in-out;
+  opacity: ${(props) => (props.horizontalDash ? "1" : "0")};
+  visibility: ${(props) => (props.horizontalDash ? "visibility" : "hidden")};
+`;
+
+export const MenuBackGround = styled.div`
+  ${({ horizontalDash }) =>
+    horizontalDash &&
+    css`
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.9);
+      transition: background-color 0.5s;
+    `}
+`;
+
+// MenuBar
+
+export const Ul = styled.ul``;
+export const Li = styled.li`
+  font-size: 30px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  color: rgb(238, 238, 238);
+  font-family: "Poppins", sans-serif;
+  transition: 0.5s;
+  &:hover {
+    transition: 0.5s;
+    color: rgba(238, 238, 238, 0.4);
+    /* rgb() */
+  }
+`;
