c4c38039ef5c21da75956717d43471dfab4cdb38
Kamil Duliniec <123dulko@wp.pl>
2021-06-28 12:59:33 +0200

    set menu after click on menu and styled it

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,34 @@
-import React, {useState} from "react";
-import { NavContainer, NavBurger, Burger, NavLogo } from "./style";
+import React, { useState } from "react";
+import MenuBar from "./MenuBar";
+import {
+  NavContainer,
+  NavBurger,
+  Burger,
+  NavLogo,
+  Module,
+  MenuBackGround,
+} from "./style";
 
 const Nav = () => {
-const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <NavContainer>
-      <NavLogo>logo</NavLogo>
-      <NavBurger onClick={() => setMenuOpen(!menuOpen)}>
-        <Burger horizontalDash={menuOpen}></Burger>
-      </NavBurger>
-    </NavContainer>
+    <>
+      <NavContainer>
+        <NavLogo>logo</NavLogo>
+        <NavBurger onClick={handleOpen}>
+          <Burger horizontalDash={menuOpen}></Burger>
+        </NavBurger>
+        <Module horizontalDash={menuOpen}>
+          <MenuBar />
+        </Module>
+      </NavContainer>
+      <MenuBackGround horizontalDash={menuOpen}></MenuBackGround>
+    </>
   );
 };
 
